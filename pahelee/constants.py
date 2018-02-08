c6af67f714bb7bc921f159058a771ca33ceb2ff0
sujan poudel <spoudel347@gmail.com>
2018-02-08 18:17:16 +0545

    header added

--- pahelee/constants.py
+++ pahelee/constants.py
@@ -1,3 +1,12 @@
+'''
+File: constants.py
+Created Date: Thursday February 8th 2018
+Author: Sujan Poudel
+Github: https://github.com/psuzn
+Last Modified:Thursday, February 8th 2018, 6:16:32 pm
+Copyright (c) 2018 https://github.com/psuzn
+'''
+
 UNVISITED_BACKGROUND=[0.0, 0.52, 1.0] # background color for the unvisited cells
 VISITED_BACKGROUND=[1.0,1.0,1.0] # for visited cells
 CURRENT_BACKGROUND=[0.16, 0.41, 0.31] # for visiter cell
