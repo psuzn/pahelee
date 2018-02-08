167f7174a13d36cbe1a9e35489adf0103ed6c391
sujan poudel <spoudel347@gmail.com>
2018-02-08 18:17:07 +0545

    header added

---
+++ pahelee/pahelee.py
@@ -0,0 +1,16 @@
+'''
+File: pahelee.py
+Created Date: Tuesday February 6th 2018
+Author: Sujan Poudel
+Github: https://github.com/psuzn
+Last Modified:Tuesday, February 6th 2018, 9:38:35 pm
+Copyright (c) 2018 https://github.com/psuzn
+'''
+
+from OpenGL.GLUT import *
+from OpenGL.GL import *
+from OpenGL.GLU import *
+from lib import *
+import sys
+import time 
+import random
