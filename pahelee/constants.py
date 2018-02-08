87e9beaef349d374bfa503c442cf478822e04d99
sujan poudel <spoudel347@gmail.com>
2018-02-08 17:34:41 +0545

    constants added

---
+++ pahelee/constants.py
@@ -0,0 +1,5 @@
+UNVISITED_BACKGROUND=[0.0, 0.52, 1.0] # background color for the unvisited cells
+VISITED_BACKGROUND=[1.0,1.0,1.0] # for visited cells
+CURRENT_BACKGROUND=[0.16, 0.41, 0.31] # for visiter cell
+WALLCOLOR=[0.0,0.0,0.0] # color for the wall color
+WALLWIDTH=2 #width of wall
