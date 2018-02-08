7847706ad820834ada52387969170a351856db71
sujan poudel <spoudel347@gmail.com>
2018-02-08 17:38:18 +0545

    line () function added

--- pahelee/lib.py
+++ pahelee/lib.py
@@ -3,7 +3,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from constants import *
 
-def line(x1,y1,x2,y2,visited=False):
+def line(x1,y1,x2,y2):
     glLineWidth(WALLWIDTH)
     glBegin(GL_LINES)
     glVertex2f(x1, y1)
