d7f4b5d576280f4cc5f82ce2860651e2cadd72ab
sujan poudel <spoudel347@gmail.com>
2018-02-08 19:06:39 +0545

    upto grid show in screen

--- pahelee/pahelee.py
+++ pahelee/pahelee.py
@@ -16,6 +16,10 @@ import sys
 import time 
 import random
 
+# 
+GRID_WIDTH=8
+GRID_HEIGHT=8
+# 
 
 grid=[]
 current=None
@@ -33,8 +37,15 @@ def initializeGrid():
     current.visited=True
 
 def loop():
-    pass
+    showGrid()
+    return
 
+def updateConstants():
+    global GRID_WIDTH,GRID_HEIGHT
+    GRID_WIDTH=glutGet(GLUT_WINDOW_WIDTH)/GRID_COLUMNS
+    GRID_HEIGHT=glutGet(GLUT_WINDOW_HEIGHT)/GRID_ROWS
+    Cell.height=GRID_HEIGHT
+    Cell.width=GRID_WIDTH
 
 def display(val=None):
     glutTimerFunc(int(1000/FPS), display,1)
@@ -42,6 +53,24 @@ def display(val=None):
     loop()
     glutSwapBuffers()
     return
+
+def showGrid():
+    global grid
+    for rows in grid:
+        for cel in rows:
+            cel.show()
+    setColor(CURRENT_BACKGROUND)
+    current.show(backgroundColor=CURRENT_BACKGROUND)
+
+def resize( width, height):
+    global WINDOW_HEIGHT,WINDOW_WIDTH
+    glViewport(0,0,width,height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    updateConstants()
+    gluOrtho2D(0, GLdouble (width), 0, GLdouble (height) )
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
 
 def initializeOPENGL():
     glutInit(sys.argv)
