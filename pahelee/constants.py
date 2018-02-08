d5e8926dd16ca2856d027d1121ab934c35331dd4
sujan poudel <spoudel347@gmail.com>
2018-02-08 17:44:22 +0545

    constants for walls added

--- pahelee/constants.py
+++ pahelee/constants.py
@@ -3,3 +3,10 @@ VISITED_BACKGROUND=[1.0,1.0,1.0] # for visited cells
 CURRENT_BACKGROUND=[0.16, 0.41, 0.31] # for visiter cell
 WALLCOLOR=[0.0,0.0,0.0] # color for the wall color
 WALLWIDTH=2 #width of wall
+
+# walls
+TOP=0
+RIGHT=1
+BOTTOM=2
+LEFT=3
+#end walls
