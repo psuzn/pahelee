2b5c911159d195d6a6aab0685cfe64d319b2975b
sujan poudel <spoudel347@gmail.com>
2018-02-08 17:23:09 +0545

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+pahelee
+===============================
+
+version number: 0.0.1
+author: Sujan Poudel
+
+Overview
+--------
+
+Maze generator using recursive depth first search algorithm
+
+Installation / Usage
+--------------------
+
+To install use pip:
+
+    $ pip install pahelee
+
+
+Or clone the repo:
+
+    $ git clone https://github.com/psuzn/pahelee.git
+    $ python setup.py install
+    
+Contributing
+------------
+
+TBD
+
+Example
+-------
+
+TBD
---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+__version__ = '0.0.1'
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+# get the dependencies and installs
+with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+
+install_requires = [x.strip() for x in all_reqs if 'git+' not in x]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs if x.startswith('git+')]
+
+setup(
+    name='pahelee',
+    version=__version__,
+    description='Maze generator using recursive depth first search algorithm',
+    long_description=long_description,
+    url='https://github.com/psuzn/pahelee',
+    download_url='https://github.com/psuzn/pahelee/tarball/' + __version__,
+    license='BSD',
+    classifiers=[
+      'Development Status :: 3 - Alpha',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3',
+    ],
+    keywords='',
+    packages=find_packages(exclude=['docs', 'tests*']),
+    include_package_data=True,
+    author='Sujan Poudel',
+    install_requires=install_requires,
+    dependency_links=dependency_links,
+    author_email='Spoudel347@gmail.com'
+)
